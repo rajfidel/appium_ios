e0eccc40f9962f13379fb9cf175c5a09761279d0
kunnath raj <fraj@fastest.cc>
2017-02-16 00:16:43 -0800

    Initial commit

---
+++ appium_uicatalog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.iOS;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Appium.PageObjects;
+using OpenQA.Selenium.Remote;
+using System.Threading;
+
+namespace appium_uicatalog
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+		}
+	}
+}
